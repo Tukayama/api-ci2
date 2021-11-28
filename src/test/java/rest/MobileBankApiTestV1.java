65f38d272429635492f0d5a435a6a42e7e0005b9
Tukayama <ya.tumanov2009@yandex.ru>
2021-11-28 17:07:34 +0300

    init commit

--- src/test/java/rest/MobileBankApiTestV1.java
+++ src/test/java/rest/MobileBankApiTestV1.java
@@ -1,7 +1,12 @@
 package rest;
 
 import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
 public class MobileBankApiTestV1 {
@@ -16,7 +21,11 @@ public class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+        .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0));
 
     }
 }
